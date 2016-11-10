a5db3a9b14036aeb8bbf97f2700fa1fec463d35a
smeth194243 <smor7432@740s-pl-114.local>
2016-11-10 13:42:38 -0700

    Added Frame stuff

--- src/array/controller/ArrayController.java
+++ src/array/controller/ArrayController.java
@@ -1,20 +1,25 @@
 package array.controller;
 
 import array.model.Hipster;
-//import array.view.HipsterFrame;
+import array.view.HipsterFrame;
 
 public class ArrayController
 {	
-	
+	private HipsterFrame appFrame;
 	private int [] numbers;
 	private String [] words = {"This ","is the ","second hipster","level: ","initialization","lists"};
 	private Hipster [] hipsters;
 	
 	public ArrayController()
 	{
-		numbers = new int[20];
-		
+		thirdLevelHipster();
+		appFrame = new HipsterFrame(this);
 		hipsters = new Hipster[6];
+	}
+	
+	private void thirdLevelHipster()
+	{
+		numbers = new int [] {0,1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1,0};
 	}
 	public void start()
 	{
@@ -37,5 +42,20 @@ public class ArrayController
 		{
 			System.out.print(tempInt + ", ");
 		}
+	}
+	
+	public String[] getWords()
+	{
+		return words;
+	}
+	
+	public int[] getNumbers()
+	{
+		return numbers;
+	}
+	
+	public Hipster[] getHipsters()
+	{
+		return hipsters;
 	}
 }
--- src/array/view/HipsterFrame.java
+++ src/array/view/HipsterFrame.java
@@ -1,6 +1,31 @@
 package array.view;
 
-public class HipsterFrame
+import javax.swing.JFrame;
+import array.controller.ArrayController;
+import java.awt.Dimension;
+
+public class HipsterFrame extends JFrame
 {
+	
+	private ArrayController baseController;
+	private HipsterPanel basePanel;
+	
+	public HipsterFrame(ArrayController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		basePanel = new HipsterPanel(baseController);
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(basePanel);
+		this.setTitle("Hipsters 2016");
+		this.setSize(new Dimension(600,400));
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 
 }
